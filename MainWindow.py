279180b2d716cd6ca78f55fb664b1f948be7cf8f
tw1zzR <regenuss@gmail.com>
2025-04-19 18:56:32 +0300

    Fixed LAB4

--- MainWindow.py
+++ MainWindow.py
@@ -3,7 +3,6 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from calculate_methods import *
 
-
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
@@ -12,6 +11,8 @@ class MainWindow(QMainWindow):
         self.transfer_time = QLabel(self)
 
         self.calculate_btn = QPushButton("CALCULATE", self)
+        self.file_size_input = QLineEdit(self)
+        self.channel_capacity_input = QLineEdit(self)
 
         self.layout = QVBoxLayout()
         self.container = QWidget()
@@ -20,12 +21,16 @@ class MainWindow(QMainWindow):
 
     def initUI(self):
         self.setWindowTitle("Transfer Info Calculator")
-        self.setGeometry(1100, 500, 300, 200)
+        self.setGeometry(1100, 500, 300, 250)
         self.setWindowIcon(QIcon("icon.png"))
 
         self.calculate_btn.clicked.connect(self.calculate_and_display)
         self.calculate_btn.setFixedHeight(50)
 
+        self.layout.addWidget(QLabel("File Size (KB):", self), alignment=Qt.AlignCenter)
+        self.layout.addWidget(self.file_size_input, alignment=Qt.AlignCenter)
+        self.layout.addWidget(QLabel("Channel Capacity (bps):", self), alignment=Qt.AlignCenter)
+        self.layout.addWidget(self.channel_capacity_input, alignment=Qt.AlignCenter)
         self.layout.addWidget(self.info_loss, alignment=Qt.AlignCenter)
         self.layout.addWidget(self.transfer_time, alignment=Qt.AlignCenter)
         self.layout.addWidget(self.calculate_btn)
@@ -46,6 +51,14 @@ class MainWindow(QMainWindow):
                 background-color: rgb(95, 237, 107);
                 border: 2px solid rgb(62, 130, 68);
             }
+            QLineEdit {
+                font-size: 16px;
+                font-weight: bold;
+                font-family: Arial;
+                border: 2px solid rgb(62, 130, 68);
+                padding: 5px;
+                background-color: rgb(240, 240, 240);
+            }
             QMainWindow {
                 font-size: 16px;
                 font-weight: bold;
@@ -63,7 +76,10 @@ class MainWindow(QMainWindow):
         ])
 
         info_loss = calculate_information_loss(p_b_a)
-        transfer_time = calculate_transfer_time(100, 100_000_000)
+
+        file_size_kb = float(self.file_size_input.text())
+        channel_capacity_bps = float(self.channel_capacity_input.text())
+        transfer_time = calculate_transfer_time(file_size_kb, channel_capacity_bps)
 
         self.info_loss.setText(f"Information Loss: {info_loss:.4f} bits")
         self.transfer_time.setText(f"Transfer Time: {transfer_time:.6f} seconds")
